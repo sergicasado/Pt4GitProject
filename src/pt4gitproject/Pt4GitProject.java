afa4dee08691c15ea6ea0e26a674b198d67a9a82
Sergi Casado <sergicasado1@iesebre.com>
2021-02-10 19:09:29 +0100

    Pujo el codi actualitzat

--- src/pt4gitproject/Pt4GitProject.java
+++ src/pt4gitproject/Pt4GitProject.java
@@ -15,6 +15,9 @@ public class Pt4GitProject {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
+        
+        
+        System.out.println("MODIFICACIO CODI 1");
         // TODO code application logic here
     }
     
